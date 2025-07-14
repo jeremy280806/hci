a6746e15c62e0efe8a2ea1c3a2fc674feed823ee
jeremy280806 <jeremyarden2808@gmail.com>
2025-07-14 12:04:47 +0700

    Membuat sidebar

--- asset/js/main.js
+++ asset/js/main.js
@@ -15,134 +15,160 @@ function toggleMenu(){
 // get event form element
 var eventForm = document.getElementById('event_form');
 
-// detect when form  is submited 
-eventForm.addEventListener('submit', function(e){
-    e.preventDefault();
 
-    // get email value
-    var email = document.getElementById('email_address').value;
+// // detect when form  is submited 
+// eventForm.addEventListener('submit', function(e){
+//     e.preventDefault();
 
-    // get full name value
-    var fullname = document.getElementById('full-name').value;
+//     // get email value
+//     var email = document.getElementById('email_address').value;
 
-    // get birth date value
-    var birthdate = document.getElementById('birth_date').value;
+//     // get full name value
+//     var fullname = document.getElementById('full-name').value;
 
-    // get gender value
-    var gender = document.getElementById('gender').value;
+//     // get birth date value
+//     var birthdate = document.getElementById('birth_date').value;
 
-    // get Tos 
-    var tos = document.getElementById('tos').value;
+//     // get gender value
+//     var gender = document.getElementById('gender').value;
 
-    var message = '';
-    var formValid = false;
+//     // get Tos 
+//     var tos = document.getElementById('tos').value;
 
-    // check if all form is filled
-    if(email == '' || fullname == '' || birthdate == '' || gender == '' || tos == ''){
-        // check if certain form is unfilled
-        message = 'Please fill all forms!';
-    }
-    else{
-        // check if all forms if filled
+//     var message = '';
+//     var formValid = false;
 
-        // check email form
-        if(email.length <= 5){
-            // check if email less than 5 characters
-            message = 'Please fill the email form correctly!';
-        }
-        else{
-            // if email more than 5 characters
-            if(email.includes('@')){
-                // if email includes @ characters
-                formValid = true;
-            }
-            else{
-                // if email not includes @ character
-                message = 'Email value must contain @ character'
-                formValid = false;
-            }
-        }
+//     // check if all form is filled
+//     if(email == '' || fullname == '' || birthdate == '' || gender == '' || tos == ''){
+//         // check if certain form is unfilled
+//         message = 'Please fill all forms!';
+//     }
+//     else{
+//         // check if all forms if filled
 
-        // check full name form 
-        if(fullname.length <= 5){
-            // check is full name is less than 5 characters
-            message = 'Please fill name form correctly';
-            formValid = false;
-        }
-        else{
-            // check if full name more than 5 characters
-            if(formValid == true){
-                formValid = true;
-            }
-        }
+//         // check email form
+//         if(email.length <= 5){
+//             // check if email less than 5 characters
+//             message = 'Please fill the email form correctly!';
+//         }
+//         else{
+//             // if email more than 5 characters
+//             if(email.includes('@')){
+//                 // if email includes @ characters
+//                 formValid = true;
+//             }
+//             else{
+//                 // if email not includes @ character
+//                 message = 'Email value must contain @ character'
+//                 formValid = false;
+//             }
+//         }
 
-        // check birth date
-        if(new Date(birthdate).getTime() <= new Date().getTime()){
-            // check if birth date less than current date
-            if(formValid == true){
-                formValid = true;
-            }
-        }
-        else{
-            // check if birth date more than current date (invalid)
-            formValid = false;
-            message = ' Please fill the correc birth date!';
-        }
+//         // check full name form 
+//         if(fullname.length <= 5){
+//             // check is full name is less than 5 characters
+//             message = 'Please fill name form correctly';
+//             formValid = false;
+//         }
+//         else{
+//             // check if full name more than 5 characters
+//             if(formValid == true){
+//                 formValid = true;
+//             }
+//         }
 
-        // check agree value
-        if(tos == 'agree'){
-            if(formValid == true){
-                formValid = true;
-            }
-        }
-        else{
-            formValid = false;
-            message = 'Please click agree before you submit the data';
-        }
+//         // check birth date
+//         if(new Date(birthdate).getTime() <= new Date().getTime()){
+//             // check if birth date less than current date
+//             if(formValid == true){
+//                 formValid = true;
+//             }
+//         }
+//         else{
+//             // check if birth date more than current date (invalid)
+//             formValid = false;
+//             message = ' Please fill the correc birth date!';
+//         }
 
-        if(formValid){
-            // if all forms is valid 
-            message = ' Successfully submit data!';
-        }
-    }
-    alert(message);
+//         // check agree value
+//         if(tos == 'agree'){
+//             if(formValid == true){
+//                 formValid = true;
+//             }
+//         }
+//         else{
+//             formValid = false;
+//             message = 'Please click agree before you submit the data';
+//         }
 
-});
+//         if(formValid){
+//             // if all forms is valid 
+//             message = ' Successfully submit data!';
+//         }
+//     }
+//     alert(message);
 
-// detect if user click agree
-var agreeButton = document.getElementById('agree_area');
+// });
 
-// detect if user click disagree
-var disagreeButton = document.getElementById('disagree_area');
+// // detect if user click agree
+// var agreeButton = document.getElementById('agree_area');
 
-// detect when user click the agree area
-agreeButton.addEventListener('click', function(e){
-    e.preventDefault();
+// // detect if user click disagree
+// var disagreeButton = document.getElementById('disagree_area');
 
-    // get agree area value
-    var agreeValue = agreeButton.getAttribute('data-value');
+// // detect when user click the agree area
+// agreeButton.addEventListener('click', function(e){
+//     e.preventDefault();
+
+//     // get agree area value
+//     var agreeValue = agreeButton.getAttribute('data-value');
      
-    // assign agree value into ToS hidden form
-    var tos = document.getElementById('tos');
-    tos.value = agreeValue; 
+//     // assign agree value into ToS hidden form
+//     var tos = document.getElementById('tos');
+//     tos.value = agreeValue; 
     
-    agreeButton.classList.add('tos-agree');
-    disagreeButton.classList.remove('tos-disagree');
-});
+//     agreeButton.classList.add('tos-agree');
+//     disagreeButton.classList.remove('tos-disagree');
+// });
 
 
-// detect when user click the disagree area
-disagreeButton.addEventListener('click', function(e){
-    e.preventDefault();
+// // detect when user click the disagree area
+// disagreeButton.addEventListener('click', function(e){
+//     e.preventDefault();
 
-    // get disagree area value
-    var disagreeValue = disagreeButton.getAttribute('data-value');
+//     // get disagree area value
+//     var disagreeValue = disagreeButton.getAttribute('data-value');
      
-    // assign disagree value into ToS hidden form
-    var tos = document.getElementById('tos');
-    tos.value = disagreeValue; 
+//     // assign disagree value into ToS hidden form
+//     var tos = document.getElementById('tos');
+//     tos.value = disagreeValue; 
 
-    agreeButton.classList.remove('tos-agree');
-    disagreeButton.classList.add('tos-disagree');
+//     agreeButton.classList.remove('tos-agree');
+//     disagreeButton.classList.add('tos-disagree');
+// });
+
+// membuat sidebar
+$('.sidebar-toggle').click(function(){
+    if($('.sidebar').hasClass('active-sidebar')){
+        // tutup sidebar
+        $('.sidebar').removeClass('active-sidebar');
+        $('.main-sidebar').animate({
+            left: '-20rem',
+        }, 300);
+
+        $('.sidebar-toggle').animate({
+            left: '0rem',
+        }, 300);
+
+    }else{
+        // buka sidebar
+        $('.sidebar').addClass('active-sidebar');
+        $('.main-sidebar').animate({
+            left: '0rem',
+        }, 300);
+
+        $('.sidebar-toggle').animate({
+            left: '20rem',
+        }, 300);
+    }
 });
-
